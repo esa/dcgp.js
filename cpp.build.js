45258425fb7371481ac920194acdb69041b6bf3c
mikeheddes <mikeheddes@gmail.com>
2019-03-10 22:03:42 +0100

    Make scripts cross env closes #5

---
+++ cpp.build.js
@@ -0,0 +1,40 @@
+/* eslint-env node */
+/* eslint-disable no-console */
+const execa = require('execa');
+const glob = require('glob');
+
+glob('src/bindings/*.cpp', null, async (error, files) => {
+  if (error) {
+    throw new Error(error);
+  }
+
+  const cwd = process.cwd();
+
+  let args = [
+    '-o',
+    'dcgp.js',
+    `-I${cwd}/dcgp/include/`,
+    `-I${cwd}/audi/include/`,
+    '-std=c++11',
+    '-g4',
+    '--source-map-base',
+    'http://localhost:8080/',
+    '-O3',
+    '--pre-js',
+    `${cwd}/src/pre.js`,
+  ];
+
+  args = files.concat(args);
+
+  try {
+    const { stdout, stderr } = await execa('emcc', args);
+
+    if (stderr) {
+      console.error(stderr);
+    }
+
+    console.log(stdout);
+  } catch (error) {
+    throw error;
+  }
+});
--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,62 @@
 import babel from 'rollup-plugin-babel';
 import copy from 'rollup-plugin-copy';
 import pkg from './package.json';
+import fs from 'fs';
+import path from 'path';
+
+function emptyDir(outputDir) {
+  return {
+    name: 'empty-lib-dir',
+    buildStart() {
+      const absolutePath = path.resolve(outputDir);
+
+      return new Promise(resolve => {
+        resolve(fs.statSync(absolutePath));
+      }).then(stats => {
+        if (!stats.isDirectory) {
+          Promise.reject('outputDir must be a directory');
+        }
+
+        const files = fs.readdirSync(absolutePath);
+
+        for (let index = 0; index < files.length; index++) {
+          fs.unlinkSync(path.join(absolutePath, files[index]));
+        }
+
+        return files;
+      });
+    },
+  };
+}
+
+const inputDir = 'src';
+const outputDir = 'lib';
 
 export default {
-  input: 'src/index.js',
+  input: path.join(inputDir, 'index.js'),
   external: [
     ...Object.keys(pkg.dependencies || {}),
     ...Object.keys(pkg.peerDependencies || {}),
   ],
   plugins: [
+    emptyDir(outputDir),
     babel(),
     copy({
-      'dcgp.wasm': 'lib/dcgp.wasm',
-      'dcgp.wasm.map': 'lib/dcgp.wasm.map',
-      'src/index.html': 'lib/index.html',
+      'dcgp.wasm': path.join(outputDir, 'dcgp.wasm'),
+      'dcgp.wasm.map': path.join(outputDir, 'dcgp.wasm.map'),
+      [path.join(inputDir, 'index.html')]: path.join(outputDir, 'index.html'),
     }),
   ],
   output: [
     {
       format: 'umd',
-      file: 'lib/dcgp.umd.js',
+      file: path.join(outputDir, 'dcgp.umd.js'),
       name: 'dcgp',
       exports: 'named',
     },
     {
       format: 'es',
-      file: 'lib/dcgp.es.js',
+      file: path.join(outputDir, 'dcgp.es.js'),
     },
   ],
 };
