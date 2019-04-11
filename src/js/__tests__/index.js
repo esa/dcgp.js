be1077903ca81e1ba157f017503620631a3540a6
mikeheddes <mikeheddes@gmail.com>
2019-04-11 11:54:18 +0200

    Update test

--- src/js/__tests__/index.js
+++ src/js/__tests__/index.js
@@ -1,18 +1,18 @@
-const fs = require('fs');
-const path = require('path');
-import { createInstance } from '../';
+const fs = require('fs')
+const path = require('path')
+import { initialise, KernelSet, Expression, algorithms } from '../'
 
-let dcgpBuffer;
-const inputs = [[0, 1], [1, 1], [2, 1], [3, 1], [4, 1]];
-const outputs = [[2], [4], [6], [8], [10]];
+let dcgpBuffer
+const inputs = [[0, 1], [1, 1], [2, 1], [3, 1], [4, 1]]
+const outputs = [[2], [4], [6], [8], [10]]
 
 beforeAll(async () => {
-  dcgpBuffer = fs.readFileSync(path.resolve('dcgp.wasm')).buffer;
-});
+  dcgpBuffer = fs.readFileSync(path.resolve('dcgp.wasm')).buffer
+})
 
 it('instantiates without error', async () => {
-  const dcgp = await createInstance(dcgpBuffer);
-  const myKernelSet = new dcgp.KernelSet(['sum', 'diff', 'mul', 'pdiv']);
-  const myExpression = new dcgp.Expression(2, 1, 2, 6, 5, 2, myKernelSet, 5);
-  dcgp.algorithms.onePlusLambda(myExpression, 5, 50, inputs, outputs);
-});
+  await initialise(dcgpBuffer)
+  const myKernelSet = new KernelSet(['sum', 'diff', 'mul', 'pdiv'])
+  const myExpression = new Expression(2, 1, 2, 6, 5, 2, myKernelSet, 5)
+  algorithms.muPlusLambda(myExpression, 1, 5, 50, inputs, outputs)
+})
